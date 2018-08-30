e57875628498c69609a07b48a47de53e3d4f9d76
kyuhyoung <kyuhyoung@gmail.com>
2018-08-30 13:55:09 +0900

    Add a function 'what_time_in_seconds_is_it_now()'

--- util_cpp.cpp
+++ util_cpp.cpp
@@ -7,3 +7,19 @@ string python_join_equivalent(const string& dir_to_file, const string& filename)
   std::experimental::filesystem::path full_path = dir / fn;
   return full_path.u8string();                                                                               
 }
+
+
+//  original code : https://github.com/pjreddie/darknet/blob/master/src/utils.c
+#include <sys/time.h>
+#include <unistd.h>
+double what_time_in_seconds_is_it_now()                                                                       
+{                                   
+	struct timeval time;
+	if (gettimeofday(&time,NULL)) 
+	{
+		return 0;
+	}
+	return (double)time.tv_sec + (double)time.tv_usec * .000001;
+}
+
+

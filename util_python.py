91bc0a281ebd7a39fa2e9254ac79ca661032e771
kyuhyoung <kyuhyoung@gmail.com>
2019-05-28 15:26:23 +0900

    Update util_python.py

--- util_python.py
+++ util_python.py
@@ -201,7 +201,7 @@ def extract_file_extension(path_file, with_dot):
 
 import os
 def get_immediate_subdirectories(direc):
-    return [name for name in os.listdir(direc) if os.path.isdir(os.path.join(direc, name))]
+    return [os.path.join(direc, name) for name in os.listdir(direc) if os.path.isdir(os.path.join(direc, name))]
 
 ###################################################################################################################
 #   image related

ac6cbb139fb9206d4d72d56d5f9a2c27f2684e9c
kyuhyoung <kyuhyoung@gmail.com>
2020-08-20 19:11:51 +0900

    Update util_cpp.cpp

--- util_cpp.cpp
+++ util_cpp.cpp
@@ -2904,6 +2904,48 @@ vector<Point> find_peaks_2D(const vector<Point2f>& li_center, const Size& sz_im,
     return li_center_max;        
 }
 
+
+Mat rot_mat_2_quaternion(const Mat& m33) {
+
+    double qx, qy, qz, qw,  
+        m00 = m33.at<double>(0, 0), m01 = m33.at<double>(0, 1), m02 = m33.at<double>(0, 2),
+        m10 = m33.at<double>(1, 0), m11 = m33.at<double>(1, 1), m12 = m33.at<double>(1, 2),
+        m20 = m33.at<double>(2, 0), m21 = m33.at<double>(2, 1), m22 = m33.at<double>(2, 2);
+
+    float tr = m00 + m11 + m22;
+
+    if (tr > 0) { 
+        float S = sqrt(tr+1.0) * 2; // S=4*qw 
+    qw = 0.25 * S;
+      qx = (m21 - m12) / S;
+        qy = (m02 - m20) / S; 
+          qz = (m10 - m01) / S; 
+          } else if ((m00 > m11)&(m00 > m22)) { 
+            float S = sqrt(1.0 + m00 - m11 - m22) * 2; // S=4*qx 
+              qw = (m21 - m12) / S;
+                qx = 0.25 * S;
+                  qy = (m01 + m10) / S; 
+                    qz = (m02 + m20) / S; 
+                    } else if (m11 > m22) { 
+                      float S = sqrt(1.0 + m11 - m00 - m22) * 2; // S=4*qy
+                        qw = (m02 - m20) / S;
+                          qx = (m01 + m10) / S; 
+                            qy = 0.25 * S;
+                              qz = (m12 + m21) / S; 
+                              } else { 
+                                float S = sqrt(1.0 + m22 - m00 - m11) * 2; // S=4*qz
+                                  qw = (m10 - m01) / S;
+                                    qx = (m02 + m20) / S;
+                                      qy = (m12 + m21) / S;
+                                        qz = 0.25 * S;
+                                        }
+    
+    Mat Q = (Mat_<double>(4, 1) << qx, qy, qz, qw);
+    //Mat Q = (Mat_<double>(4, 1) << qw, qx, qy, qz);
+    return Q;
+}    
+
+
 double sqrt_of_mean_square(const cv::Mat& m)
 {
 	double sos = sum_of_squared(m);

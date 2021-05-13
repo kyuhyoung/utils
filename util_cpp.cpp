8b56dcf7a1f23b4ce184a968f4be737d44adfe30
kyuhyoung <kyuhyoung@gmail.com>
2021-05-13 19:04:07 +0900

    Update util_cpp.cpp

--- util_cpp.cpp
+++ util_cpp.cpp
@@ -2782,6 +2782,31 @@ std::vector<std::size_t> sort_permutation(
 }
 
 
+template <typename T>
+void apply_permutation_in_place(
+    std::vector<T>& vec,
+    const std::vector<std::size_t>& p)
+{
+    std::vector<bool> done(vec.size());
+    for (std::size_t i = 0; i < vec.size(); ++i)
+    {
+        if (done[i])
+        {
+            continue;
+        }
+        done[i] = true;
+        std::size_t prev_j = i;
+        std::size_t j = p[i];
+        while (i != j)
+        {
+            std::swap(vec[prev_j], vec[j]);
+            done[j] = true;
+            prev_j = j;
+            j = p[j];
+        }
+    }
+}
+
 
 //-----------------------------------------------------------------------------------------------------  
 //	vector<int> li_int({4, 1, 3, 2});

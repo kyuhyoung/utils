baac0f02e4f212681f0af7c17b4435a7d549f382
kyuhyoung <kyuhyoung@gmail.com>
2022-01-03 16:45:33 +0900

    Update util_cpp.cpp

--- util_cpp.cpp
+++ util_cpp.cpp
@@ -583,6 +583,35 @@ inline double unwrap(double ang_rad_pre, double ang_rad_new)
 }
 
 
+Mat eulerAnglesToRotationMatrix( const Vec3f& angle, bool from_degree )
+{
+    Vec3f theta = angle;
+    if(from_degree)
+    {
+        theta[0] = deg2rad(angle[0]);   theta[1] = deg2rad(angle[1]);   theta[2] = deg2rad(angle[2]);
+    }
+    // Calculate rotation about x axis
+    Mat R_x = ( Mat_<double>( 3, 3 ) <<
+                1,       0,              0,
+                0,       cos( theta[0] ),   -sin( theta[0] ),
+                0,       sin( theta[0] ),   cos( theta[0] )
+              );
+    // Calculate rotation about y axis
+    Mat R_y = ( Mat_<double>( 3, 3 ) <<
+                cos( theta[1] ),    0,      sin( theta[1] ),
+                0,               1,      0,
+                -sin( theta[1] ),   0,      cos( theta[1] )
+              );
+    // Calculate rotation about z axis
+    Mat R_z = ( Mat_<double>( 3, 3 ) <<
+                cos( theta[2] ),    -sin( theta[2] ),      0,
+                sin( theta[2] ),    cos( theta[2] ),       0,
+                0,               0,                  1 );
+    // Combined rotation matrix
+    Mat R = R_z * R_y * R_x;
+    return R;
+}
+
 
 // Calculates rotation matrix to euler angles
 // The result is the same as MATLAB except the order
